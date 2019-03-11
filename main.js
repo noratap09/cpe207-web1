1927710652655f576cd70ac0b22d27cceeaca49c
noratap <noratap09@hotmail.com>
2019-03-11 16:12:48 +0700

    first_Web

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var temp = '';
+for(var i=1;i<=10;i++)
+{
+    console.log("data:"+i);
+    temp += i+'<br>';
+}
+
+//document.querySelector('#output').innerHTML = temp;
+document.getElementById('output').innerHTML = temp;
